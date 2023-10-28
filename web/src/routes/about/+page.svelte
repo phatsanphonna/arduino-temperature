dd902088713b882f573eefc01dc8901a890772f8
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-10-28 17:35:22 +0700

    Feat: English

--- web/src/routes/about/+page.svelte
+++ web/src/routes/about/+page.svelte
@@ -6,6 +6,21 @@
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
 		Arduino Temperature Sensor
 	</h1>
+	<p class="leading-7 [&:not(:first-child)]:mt-6">
+		โครงงานนี้ได้ศึกษาพฤติกรรมของมนุษย์ ที่ต้องการความสะดวกสบาย
+		ต้องการเทคโนโลยีที่จะช่วยอำนวยความสะดวกในชีวิตประจำวันเพื่อป้องกันโรคฮีทสโตรกโรคอันตรายที่มนุษย์ไม่ระมัดระวังตัว
+		เนื่องจากประเทศไทยช่วงระยะเวลาฤดูร้อนมีสภาพแวดล้อมอากาศโดยรอบที่มีอุณหภูมิสูงมาก
+		และมนุษย์ไม่พึงระวังตัวต่อโรคฮีทสโตรกภัยเงียบนี้ได้ ทำให้ ณ
+		ปัจจุบันมีคนเสียชีวิตจากโรคนี้เพิ่มขึ้นในทุกๆปี พวกเราจึงคิดที่จะนำ arduino มาใช้ในการช่วย
+		ในเรื่องของการเตือนอุณหภูมิอากาศภายนอกที่สูงจนอาจจะทำให้เกิดอัตรายต่อชีวิตมนุษย์ได้
+	</p>
+	<p class="leading-7 [&:not(:first-child)]:mt-6">
+		ผลการดำเนินงาน
+		พวกเราเริ่มจากการที่เรามีไอเดียที่อยากการแจ้งเตือนอุณหภูมิอากาศเพื่อลดความเสี่ยงของโรคฮีสสโตกอันตรายต่อชีวิต
+		ได้ทำการหาข้อมูลอุณหภูมิที่อัตรายต่อการเกิดโรคนี้ขึ้นได้ จากนั้นทำการต่อวงจร เขียนโค้ดและใช้
+		MQTT ในการเชื่อมต่อการสื่อสารระหว่างarduino และของ Websiteราตามที่เราได้หาข้อมูล
+		เกิดข้อผิดพลาดระหว่างการทำงานเล็กน้อยแต่พวกเราสามารถแก้ไขปัญหาและสามารถทำชิ้นงานนี้ได้สำเร็จตามเวลาที่กำหนด
+	</p>
 
 	<h2
 		class="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
@@ -51,4 +66,26 @@
 	>
 		วิดีโอตัวอย่าง
 	</h2>
+	<div class="mt-10 flex justify-center items-center">
+		<iframe
+			width="560"
+			height="315"
+			src="https://www.youtube.com/embed/d_INH0gorT4?si=HlMlx1Kv57cNga_E"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		/>
+	</div>
+
+	<h2
+		class="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	>
+		สมาชิกภายในกลุ่ม
+	</h2>
+	<ul class="my-6 ml-6 list-disc [&>li]:mt-2">
+		<li><b>65070144</b> พรพัฒน์ ธนทัตธาดา</li>
+		<li><b>65070171</b> ภัทร์สรรพ์พร นครานุรักษ์</li>
+		<li><b>65070225</b> ศุภธัช สุวัฒโน</li>
+	</ul>
 </div>
